c3e336cdc720aee96aa55f0215804bfcf7e82031
Sh3yk0  (Sahid Kick) <sheyko.wk@gmail.com>
2023-04-21 18:55:04 -0600

    generate current skeleton

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,10 +1,19 @@
 const Users = require('./users.models')
-const RecoveryPasswords = require('./recoveryPasswords.models')
+const Conversations = require("./conversations.models")
+const Messages = require("./messages.models")
+const Participants = require("./participants.models")
 
 const initModels = () => {
-    //? FK = RecoveryPasswords
-    Users.hasMany(RecoveryPasswords)
-    RecoveryPasswords.belongsTo(Users)
+
+
+   //? Users -> Participants
+
+
+   //? Conversations -> Participants
+
+
+   //? Participants -> Messages
+
 }
 
 module.exports = initModels
